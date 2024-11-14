e5c03b814d058987bd994dbfe6408a1ea0fc588e
sadibhaque <sadibhaquee@gmail.com>
2024-11-14 21:10:29 +0600

    Update

---
+++ Basic/Functions/kargs.py
@@ -0,0 +1,19 @@
+def name(first, last):
+    full = f"{first} {last}"
+    return full
+
+# full_name = name('Jhon','Mendax')
+full_name = name(last='Mendax', first='Jhon')
+
+print(full_name)
+
+
+
+def famous(first, last, **extra):
+    name = f'{first} {last}'
+    print(extra)
+    return name
+
+full_name = famous('Jhon', 'Mendax', k = 'CEO')     # k is a key 
+
+print(full_name)
